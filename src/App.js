5a645416c3fbbaeb07536699e5014f51ebe2b4f1
Genevieve Clare Calma <genevieve.calma@ontario.ca>
2023-01-29 19:59:46 -0500

    fixed function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/routes/About';
 import NotFound from './components/routes/NotFound';
 import NavigationBar from './components/NavigationBar';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Router>
-          <NavigationBar />
-          <Routes>
-            <Route exact path="/" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route exact path="/" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
+  );
+  // }
 }
 
 export default App;
