2398a05afc33844f1672872b24fd7cdd3a9e4a5a
Genevieve Clare Calma <genevieve.calma@ontario.ca>
2023-01-30 14:19:15 -0500

    initialized retrieving auth user

--- src/components/routes/Profile.js
+++ src/components/routes/Profile.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { Auth } from 'aws-amplify';
 
 const Profile = () => {
+  const [user, setUser] = useState([]);
+  const [username, setUserName] = useState([]);
+
+  useEffect(() => {
+    fetchUser().then((users) => setUser(users));
+    fetchUser().then((users) => setUserName(users.username));
+  });
+
+  const fetchUser = async () => {
+    return await Auth.currentUserInfo();
+  };
+
   return (
     <div>
       <Container>
-        <h1>Profile Page Coming Soon!!!</h1>
+        <h1>{username}</h1>
       </Container>
     </div>
   );
