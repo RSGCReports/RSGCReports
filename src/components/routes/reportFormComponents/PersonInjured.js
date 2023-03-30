5626c6fbe3f2f2c80cb77dcf06dbfe5f0fe12198
rudychung <61764259+rudychung@users.noreply.github.com>
2023-03-30 12:14:59 -0400

    Updated dynamic fields

--- src/components/routes/reportFormComponents/PersonInjured.js
+++ src/components/routes/reportFormComponents/PersonInjured.js
@@ -1,58 +1,36 @@
 import React, { useEffect } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
-const PersonInjured = ({
-  index,
-  handleChange,
-  handleRemove,
-  onClickErrors,
-  errors,
-  errorSetter,
-  personInjured,
-}) => {
+const PersonInjured = ({ index, handleChange, handleRemove, onClickErrors, errorSetter }) => {
   // fix for initial empty field errors
   useEffect(() => {
     errorSetter({
-      ...errors,
-      ...{
-        [index]: {
-          ['personInjuredName']: 'Required field',
-          ['personInjuredDate']: 'Required field',
-          ['personInjuredHospital']: 'Required field',
-          ['personInjuredNatureOfInjuries']: 'Required field',
-        },
+      [index]: {
+        ['personInjuredName']: 'Required field',
+        ['personInjuredAddress']: 'Required field',
+        ['personInjuredCity']: 'Required field',
+        ['personInjuredProvince']: 'Required field',
+        ['personInjuredCountry']: 'Required field',
+        ['personInjuredPostalCode']: 'Required field',
+        ['personInjuredHospital']: 'Required field',
+        ['personInjuredNatureOfInjuries']: 'Required field',
       },
     });
   }, []);
   return (
     <>
       <hr />
-      <Row>
-        <Form.Group as={Col} controlId="formPersonInjuredName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="personInjuredName"
-            value={personInjured.personInjuredName}
-            onChange={(e) => handleChange(e, index)}
-            isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredName : null}
-          />
-          {onClickErrors[index] ? (
-            <Form.Control.Feedback type="invalid">
-              {onClickErrors[index].personInjuredName}
-            </Form.Control.Feedback>
-          ) : null}
-        </Form.Group>
-        <Form.Group as={Col} controlId="formPersonInjuredDate">
-          <Form.Label>Date of Birth</Form.Label>
-          <Form.Control
-            type="date"
-            name="personInjuredDate"
-            value={personInjured.personInjuredDate}
-            defaultValue={personInjured.personInjuredDate}
-            onChange={(e) => handleChange(e, index)}
-          />
-        </Form.Group>
-      </Row>
+      <Form.Group controlId="formPersonInjuredName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="personInjuredName"
+          onChange={(e) => handleChange(e, index)}
+          isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredName : null}
+        />
+        <Form.Control.Feedback type="invalid">
+          {onClickErrors[index] ? onClickErrors[index].personInjuredName : null}
+        </Form.Control.Feedback>
+      </Form.Group>
       <Row>
         <Form.Group as={Col} controlId="formPersonInjuredAddress">
           <Form.Label>Address</Form.Label>
@@ -61,7 +39,11 @@ const PersonInjured = ({
             name="personInjuredAddress"
             placeholder="123 Fake St"
             onChange={(e) => handleChange(e, index)}
+            isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredAddress : null}
           />
+          <Form.Control.Feedback type="invalid">
+            {onClickErrors[index] ? onClickErrors[index].personInjuredAddress : null}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} controlId="formPersonInjuredCity">
           <Form.Label>City</Form.Label>
@@ -69,7 +51,11 @@ const PersonInjured = ({
             type="text"
             name="personInjuredCity"
             onChange={(e) => handleChange(e, index)}
+            isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredCity : null}
           />
+          <Form.Control.Feedback type="invalid">
+            {onClickErrors[index] ? onClickErrors[index].personInjuredCity : null}
+          </Form.Control.Feedback>
         </Form.Group>
       </Row>
       <Row>
@@ -79,7 +65,11 @@ const PersonInjured = ({
             type="text"
             name="personInjuredProvince"
             onChange={(e) => handleChange(e, index)}
+            isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredProvince : null}
           />
+          <Form.Control.Feedback type="invalid">
+            {onClickErrors[index] ? onClickErrors[index].personInjuredProvince : null}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} controlId="formPersonInjuredCountry">
           <Form.Label>Country</Form.Label>
@@ -87,7 +77,11 @@ const PersonInjured = ({
             type="text"
             name="personInjuredCountry"
             onChange={(e) => handleChange(e, index)}
+            isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredCountry : null}
           />
+          <Form.Control.Feedback type="invalid">
+            {onClickErrors[index] ? onClickErrors[index].personInjuredCountry : null}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} controlId="formPersonInjuredPostalCode">
           <Form.Label>Postal Code</Form.Label>
@@ -96,7 +90,11 @@ const PersonInjured = ({
             name="personInjuredPostalCode"
             placeholder="X#X#X#"
             onChange={(e) => handleChange(e, index)}
+            isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredPostalCode : null}
           />
+          <Form.Control.Feedback type="invalid">
+            {onClickErrors[index] ? onClickErrors[index].personInjuredPostalCode : null}
+          </Form.Control.Feedback>
         </Form.Group>
       </Row>
       <Form.Group controlId="formPersonInjuredHospital">
@@ -104,9 +102,12 @@ const PersonInjured = ({
         <Form.Control
           type="text"
           name="personInjuredHospital"
-          value={personInjured.personInjuredHospital}
           onChange={(e) => handleChange(e, index)}
+          isInvalid={onClickErrors[index] ? onClickErrors[index].personInjuredHospital : null}
         />
+        <Form.Control.Feedback type="invalid">
+          {onClickErrors[index] ? onClickErrors[index].personInjuredHospital : null}
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formPersonInjuredNatureOfInjuries">
         <Form.Label>Nature of Injuries</Form.Label>
@@ -114,9 +115,14 @@ const PersonInjured = ({
           as="textarea"
           type="text"
           name="personInjuredNatureOfInjuries"
-          value={personInjured.personInjuredNatureOfInjuries}
           onChange={(e) => handleChange(e, index)}
+          isInvalid={
+            onClickErrors[index] ? onClickErrors[index].personInjuredNatureOfInjuries : null
+          }
         />
+        <Form.Control.Feedback type="invalid">
+          {onClickErrors[index] ? onClickErrors[index].personInjuredNatureOfInjuries : null}
+        </Form.Control.Feedback>
       </Form.Group>
       <br />
       <Button className="btn btn-danger" onClick={(e) => handleRemove(e, index)}>
