852b19fa8fff703c4a6a7ce86ef7460d9d1120c0
stefanaz2 <sfrunza@seneca.ca>
2023-04-15 21:33:28 -0400

    hopefully this works

--- src/components/routes/profileComponents/Success.js
+++ src/components/routes/profileComponents/Success.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Success = () => {
+  const reloadCount = sessionStorage.getItem('reloadCount');
+
+  const navigate = useNavigate();
+
+  const refreshPage = () => {
+    navigate(0);
+  };
+
+  useEffect(() => {
+    if (reloadCount < 2) {
+      sessionStorage.setItem('reloadCount', String(reloadCount + 1));
+      refreshPage();
+    } else {
+      sessionStorage.removeItem('reloadCount');
+    }
+  }, []);
+
   return (
     <Container className="text-center">
       <h2>Profile Completed!</h2>
